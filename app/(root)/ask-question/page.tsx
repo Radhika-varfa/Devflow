3eda5b44bbf99ade13ac733107264acf5170afbb
Radhika-varfa <rvarfa93@gmail.com>
2023-12-28 13:55:37 +0530

    initial commit

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,26 @@
+import Question from '@/components/forms/Question'
+import {auth} from '@clerk/nextjs'
+import { getUserById } from '@/lib/actions/user.action';
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+const page = async () => {
+  // const {userId} = auth();
+
+  const userId = 'CL123'
+ if(!userId) redirect('/sign-in');
+
+ const mongoUser = await getUserById({ userId });
+ console.log(mongoUser);
+
+  return (
+    <div>
+      <h1 className='h1-bold twxt-dark'>Ask a quwstion</h1>
+      <div>
+        <Question mongoUserId={JSON.stringify(mongoUser._id)}/>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ lib/utils.ts
@@ -0,0 +1,53 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const timeDifference = now.getTime() - createdAt.getTime(); // Convert to timestamps
+
+  // Define time units in milliseconds
+  const minute = 60 * 1000;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const month = 30 * day;
+  const year = 365 * day;
+
+  if (timeDifference < minute) {
+      const seconds = Math.floor(timeDifference / 1000);
+      return `${seconds} ${seconds === 1 ? 'second' : 'seconds'} ago`;
+  } else if (timeDifference < hour) {
+      const minutes = Math.floor(timeDifference / minute);
+      return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+  } else if (timeDifference < day) {
+      const hours = Math.floor(timeDifference / hour);
+      return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+  } else if (timeDifference < month) {
+      const days = Math.floor(timeDifference / day);
+      return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+  } else if (timeDifference < year) {
+      const months = Math.floor(timeDifference / month);
+      return `${months} ${months === 1 ? 'month' : 'months'} ago`;
+  } else {
+      const years = Math.floor(timeDifference / year);
+      return `${years} ${years === 1 ? 'year' : 'years'} ago`;
+  }
+};
+
+export const formatAndDivideNumber = (num: number): string => {
+    if (num >= 1000000) {
+        // For numbers greater than or equal to 1 million
+        const formattedNum = (num/1000000).toFixed(1);
+        return `${formattedNum}M`;
+    } else if (num >= 1000) {
+        // For numbers greater than or equal to 1 thousand
+        const formattedNum = (num/1000).toFixed(1);
+        return `${formattedNum}K`;
+    } else {
+        // For numbers less than 1 thousand
+        return num.toString();
+    }
+};
